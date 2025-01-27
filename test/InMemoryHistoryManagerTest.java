4b9fdf0ec880c8879fb6c8fcd8e6fab62a18459e
Asad Isokov <isokov.asad@mail.ru>
2025-01-28 01:39:31 +0300

    TZ 6

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -7,34 +7,21 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
-    private static final InMemoryHistoryManager manager = Managers.getDefaultHistory();
 
-    private static final InMemoryTaskManager manager1 = Managers.getDefault();
-
-    @Test
-    void shouldAddWithoutChanges() {
-        Task doHomeWork = new Task("Сделать дз!", "До дедлайна нужно сдать!");
-        String nameTask = doHomeWork.getName();
-        String descriptionTask = doHomeWork.getDescription();
-        doHomeWork.setId(1);
-        manager.add(doHomeWork);
-        assertEquals(nameTask, doHomeWork.getName());
-        assertEquals(1, doHomeWork.getId());
-        assertEquals(descriptionTask, doHomeWork.getDescription());
-    }
+    private static final InMemoryTaskManager manager = Managers.getDefault();
 
     @Test
     void shouldAddWorkCorrect() {
         Task doHomeWork = new Task("Сделать дз!", "До дедлайна нужно сдать!");
         Task doHomeWork1 = new Task("Сделать дз по математике!", "До дедлайна не нужно сдать!");
-        manager1.addTask(doHomeWork);
-        manager1.addTask(doHomeWork1);
-        manager1.getTask(doHomeWork.getId());
-        manager1.getTask(doHomeWork1.getId());
+        manager.addTask(doHomeWork);
+        manager.addTask(doHomeWork1);
+        manager.getTask(doHomeWork.getId());
+        manager.getTask(doHomeWork1.getId());
         ArrayList<Task> anyArray = new ArrayList<>();
         anyArray.add(doHomeWork);
         anyArray.add(doHomeWork1);
-        assertEquals(anyArray, manager1.getTasks());
+        assertEquals(anyArray, manager.getTasks());
     }
 
 }
